b08669d8cb670977aa46cfa62baf9cf4b7b68030
Klearez <krai020913@gmail.com>
2019-03-30 14:49:57 +0700

    upload image
    - using post from axios
    - sucessfully

--- client/src/_helpers/auth-header.js
+++ client/src/_helpers/auth-header.js
@@ -1,5 +1,5 @@
 export function getToken() {
-    let token = localStorage.getItem('user');
-    token = JSON.parse(token).token;
-    return token;
+    // let token = localStorage.getItem('user');
+    // token = JSON.parse(token).token;
+    return "";
 }

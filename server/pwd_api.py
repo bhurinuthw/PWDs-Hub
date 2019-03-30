73adbc2ae34b309d2a75732b33d1f76d8a988467
Iceyo Kuna <iceyokuna@hotmail.com>
2019-03-30 14:12:19 +0700

    [UPDATE] pwd_api_get-all-data

--- server/pwd_api.py
+++ server/pwd_api.py
@@ -9,13 +9,19 @@ url = 'https://accenture-hackathon-2019-11a52.firebaseio.com/' #firebase db url
 messager = firebase.FirebaseApplication(url)
 
 #register
-@app.route("/pwd", methods = ['GET','PUT'])
-def pwd():
+@app.route("/pwd", methods = ['GET'])
+def get():
     if(request.method == 'GET'):
         data = messager.get("PWD_users",None)
         response = jsonify(data)
         response.status_code = 200
         return response
+
+@app.route("/pwd/update", methods = ['PUT'])
+def update():
+    if(request.method == 'PUT'):
+        pass
+
 
 
 if __name__ == "__main__":

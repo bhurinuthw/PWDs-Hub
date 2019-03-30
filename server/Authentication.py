8c6861ba5a92143e3d9555d47bab45a98b897dc2
Parin Kobboon <punparin@gmail.com>
2019-03-30 20:31:24 +0700

    Fixed post

--- server/Authentication.py
+++ server/Authentication.py
@@ -2,6 +2,7 @@ import pyrebase
 import json
 import requests
 
+from flask import request
 
 class Authentication:
     def __init__(self):
@@ -27,7 +28,7 @@ class Authentication:
             print(error['message'])
 
     def register(self, data):
-        print(data)
+        print(type(request.get_json(silent=True)))
         auth = self.firebase.auth()
         email = data["email"]
         password = data["password"]
@@ -93,28 +94,82 @@ class Authentication:
         }
         return data
 
-if __name__ == "__main__":
-    a = Authentication()
+# if __name__ == "__main__":
+#     a = Authentication()
 
-    data = {
-        "n_id": "12345678",
-        "prefix": "นาย",
-        "name": "สวัสดี",
-        "surname": "ครับ",
-        "phone": "0811111111",
-        "email": "pwd1@gmail.com",
-        "category": ["ตาบอด", "หูหนวก"],
-        "password": "testjha11",
-        "role": "pwd"
-    }
+    # data = {
+    #     "n_id": "12345678",
+    #     "prefix": "นาย",
+    #     "name": "สวัสดี",
+    #     "surname": "ครับ",
+    #     "phone": "0811111111",
+    #     "email": "pwd1@gmail.com",
+    #     "category": ["ตาบอด", "หูหนวก"],
+    #     "password": "testjha11",
+    #     "role": "pwd"
+    # }
 
-    data = {
-        "name": "ฮ่าๆๆ",
-        "region": "กลาง",
-        "province": "กรุงเทพ",
-        "email": "com@gmail.com",
-        "password": "testjha11",
-        "role": "company"
-    }
+#     data = {
+#         "name": "ฮ่าๆๆ",
+#         "region": "กลาง",
+#         "province": "กรุงเทพ",
+#         "email": "com@gmail.com",
+#         "password": "testjha11",
+#         "role": "company"
+#     }
 
-    a.register(data)
+#     a.register(data)
+
+# import xlrd
+# import random
+# from faker import Faker
+
+# cate = ["การมองเห็น",
+#         "การได้ยิ้น",
+#         "การเคลื่อนไหว",
+#         "จิตใจ",
+#         "สติปัญญา",
+#         "การเรียนรู้",
+#         "ออทิสติก"]
+
+# loc = ("mock.xlsx")
+
+# wb = xlrd.open_workbook(loc)
+# sheet = wb.sheet_by_index(0)
+
+# print(sheet.cell_value(1, 0))
+
+# print(sheet.ncols)
+
+# fake = Faker()
+
+# for i in range(1, 100):
+#     n_id = sheet.cell_value(i, 0)
+#     prefix = sheet.cell_value(i, 1)
+#     name = sheet.cell_value(i, 2).split()[0].encode("utf8")
+#     print(name)
+#     phone = sheet.cell_value(i, 8)
+#     department = sheet.cell_value(i, 12)
+#     company = sheet.cell_value(i, 15)
+#     province = sheet.cell_value(i, 7)
+#     description = fake.text()
+#     email = fake.email()
+#     category = random.shuffle(cate)
+
+
+#     data = {
+#             "email": email,
+#             "n_id": n_id,
+#             "prefix": prefix,
+#             "name": name,
+#             "surname": surname,
+#             "phone": phone,
+#             "description": description,
+#             # "category": category,
+#             "department": department,
+#             "company": company,
+#             "img_url": "",
+#             "province": province,
+#         }
+
+#     print(data)
